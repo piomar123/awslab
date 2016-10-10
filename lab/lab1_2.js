c72170e1dcd3e2cf7edb798f6cfaf4f9f1730139
piomar123 <piomar123@gmail.com>
2016-10-10 21:44:35 +0200

    Cleared params list. Fixed express callback call with null error.

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -6,19 +6,20 @@ var params = {
   //DryRun: false,
   //Filters: [],
   //InstanceIds: [],
-  MaxResults: 1000,
-  NextToken: 'piomar123nexttoken'
+  //MaxResults: 1000,
+ // NextToken: 'piomar123nexttoken'
 };
 
 var ec2 = new AWS.EC2({});
 
 var infoTask = function(request, next){
 	ec2.describeInstances(params, function(err, data) {
-		if (err) console.log(err, err.stack); // an error occurred
-		else {
-			console.log(data);
-			next(data);
+		if (err) {
+			console.log(err, err.stack);
+			throw err;
 		}
+		console.log(data);
+		next(null, data);
 	});
 };
 
