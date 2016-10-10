a4d38b44b8e1bba46debbafc130d11f47908a14f
piomar123 <piomar123@gmail.com>
2016-10-10 18:23:12 +0200

    Added config.json. Trying to run describeInstances()

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 var lab1_1 = require("./lab/lab1_1").lab
 var example_1 = require("./example_1").lab;
+var lab02 = require("./lab/lab1_2").lab;
 
 var PORT = 8080;
 
-
 var urlMap = [
-	{path: "/", action:__dirname + "/static/index.html"},	 
-	{path: "/digest", action: lab1_1},	
-	{path: "/example_1", action: example_1}, 
+	{path: "/", action:__dirname + "/static/index.html"},
+	{path: "/digest", action: lab1_1},
+	{path: "/example_1", action: example_1},
+	{path: "/lab02/info", action: lab02.info},
 	];
 
 var service = require("./lib/service").http(urlMap);
--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -1,11 +1,27 @@
-//stub for lab 1_2
 var AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('./config.json');
 
-var task =  function(request, callback){
-	
-	
-}
+var params = {
+  //DryRun: false,
+  //Filters: [],
+  //InstanceIds: [],
+  MaxResults: 1000,
+  NextToken: 'piomar123nexttoken'
+};
 
-exports.lab = task
+var ec2 = new AWS.EC2({});
+
+var infoTask = function(request, next){
+	ec2.describeInstances(params, function(err, data) {
+		if (err) console.log(err, err.stack); // an error occurred
+		else {
+			console.log(data);
+			next(data);
+		}
+	});
+};
+
+exports.lab = {
+	info: infoTask,
+};
