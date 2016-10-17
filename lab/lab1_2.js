d4674e6ebe4df1f509bb92385e43a235dda1b4cf
piomar123 <piomar123@gmail.com>
2016-10-18 01:09:50 +0200

    Unnested callbacks hell in functions. Restored <title>.

--- lab/lab1_2.js
+++ lab/lab1_2.js
@@ -2,26 +2,20 @@ var AWS = require('aws-sdk');
 
 AWS.config.loadFromPath('./config.json');
 
-var params = {
-  //DryRun: false,
-  //Filters: [],
-  //InstanceIds: [],
-  //MaxResults: 1000,
- // NextToken: 'piomar123nexttoken'
-};
-
 var ec2 = new AWS.EC2({});
 var autoscaling = new AWS.AutoScaling();
 
 var infoTask = function(request, next){
-	ec2.describeInstances(params, function(err, data) {
+	var handleInstancesFunc = function(err, data) {
 		if (err) {
 			console.log(err, err.stack);
 			return next(err);
 		}
 		console.log(data);
 		next(null, data);
-	});
+	};
+
+	ec2.describeInstances({}, handleInstancesFunc);
 };
 
 var launchTask = function(request, next){
@@ -33,31 +27,34 @@ var launchTask = function(request, next){
 		InstanceType: 't1.micro',
 		Monitoring: {Enabled: false}
 	};
+	var tagsQuery = {
+		Tags: [{ Key: 'Name', Value: 'piotr.marcinczyk-SDK' }],
+	};
 	var output = {};
-	ec2.runInstances(runParams, function(err, runData){
+
+	var handleCreateTagsFunc = function(err, tagsData){
+		if (err) {
+			console.log(err, err.stack);
+			return next(err);
+		}
+		output.tags = tagsData;
+		console.log(tagsData);
+		next(null, output);
+	};
+
+	var handleRunInstancesFunc = function(err, runData){
 		if (err) {
 			console.log(err, err.stack);
 			return next(err);
 		}
 		console.log(runData);
 		output.run = runData;
-		ec2.createTags({
-			Resources: [runData.Instances[0].InstanceId],
-			Tags: [{
-				Key: 'Name',
-				Value: 'piotr.marcinczyk.sdk'
-			}],
-		}, function(err, tagsData){
-			if (err) {
-				console.log(err, err.stack);
-				return next(err);
-			}
-			output.tags = tagsData;
-			console.log(tagsData);
-			next(null, output);
-		});
-	});
-}
+		tagsQuery.Resources = [runData.Instances[0].InstanceId];
+		ec2.createTags(tagsQuery, handleCreateTagsFunc);
+	};
+
+	ec2.runInstances(runParams, handleRunInstancesFunc);
+};
 
 var desiredTask = function(request, next){
 	var desiredParams = {
